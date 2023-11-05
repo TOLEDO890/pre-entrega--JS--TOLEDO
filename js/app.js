9f59c1e04951208db266ba4fe9de1d62f4c0bd1b
amador toledo <amadortoledo5@gmail.com>
2023-11-05 20:55:39 -0300

    final de pre entrega

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 console.log("conectado");
 let nombrePerro = prompt("ingresa el nombre de tu perro");
-alert ("bienvenido dueño de" + " "  + nombrePerro );
-let añosHumanos = parseInt(prompt("Ingresa la edad de " + nombrePerro));
-if (añosHumanos === " "){
-    alert( "por favor ingresa solo numeros");
-}else {
-    alert("la edad humana de"+" " +nombrePerro +" "+"es" +" " +añosHumanos);
+while (!/^[A-Za-z]+$/.test(nombrePerro) || nombrePerro.trim() === "") {
+  alert(
+    "Por favor, ingresa un nombre válido que contenga solo letras y no esté vacío."
+  );
+  nombrePerro = prompt("Ingresa el nombre de tu perro nuevamente");
 }
-let añosPerros = 7;
-
+alert("bienvenido dueño de" + " " + nombrePerro);
+let añosHumanos = parseInt(prompt("Ingresa la edad de " + nombrePerro));
+while (true) {
+  if (isNaN(añosHumanos)) {
+    alert("Por favor, ingresa solo números.");
+    añosHumanos = parseInt(prompt("Ingresa la edad de " + nombrePerro));
+  } else if (parseInt(añosHumanos) <= 0) {
+    alert("Por favor, ingresa un número mayor que cero.");
+    añosHumanos = parseInt(prompt("Ingresa la edad de " + nombrePerro));
+  } else {
+    alert("La edad humana de " + nombrePerro + " es " + añosHumanos);
+    console.log("Validación completa");
+    break;
+  }
+}
 function multiplicar(a, b) {
     return a * b;
   }
-  
+let añosPerros = 7;
 let resultado = multiplicar(añosHumanos, añosPerros);
-alert("la edad real de " + " "+nombrePerro +" "+ "es"+ " "+ resultado)
 
-alert("gracias por visitar nuestro sitio web")
-   
-
-
+alert("la edad real de " + " " + nombrePerro + " " + "es" + " " + resultado);
+alert("gracias por visitar nuestro sitio web");
